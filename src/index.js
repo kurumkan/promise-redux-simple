90d5803ffa62564e0560a275b9e4592e8d996018
kurumkan <joomrise@gmail.com>
2017-02-16 17:48:51 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+export default function({dispatch}){
+    return next => action => {      
+
+        if(action.payload && action.payload.then){          
+            action.payload
+            .then((response)=>{     
+                //we use dispatch instead of next
+                //to avoid skipping other('previous') middlewares
+                //so the action will go from the beginning of the middleware chain
+                dispatch({...action, payload: response.data});
+            })
+            .catch(error=>{
+                dispatch({ ...action, payload: error, error: true });
+                return Promise.reject(error);
+            });         
+        }else{
+            next(action);
+        }       
+    }
+}
